e296c325aca007d89b5d7e028ceff5081cbf31c6
Amitesh Kumar <amitesingh@gmail.com>
2016-10-05 15:39:42 +0530

    Adding minified file to lib for importing in parent projects

--- src/opener-for-webpack.js
+++ src/opener-for-webpack.js
@@ -1,17 +1,15 @@
-var opener = require('opener');
+const opener = require('opener');
 
 export default class OpenerForWebpack {
 
   constructor(options = {}) {
-    this.options = options || {
-      url: 'http://localhost:3000'
-    };
+    this.options = options || { url: 'http://localhost:3000' };
     this.isOpen = false;
   }
 
   apply(compiler) {
-    var self = this;
-    compiler.plugin('done', function(stats) {
+    const self = this;
+    compiler.plugin('done', (stats) => {
       if (!self.isOpen && !stats.hasErrors()) {
         opener(self.options.url);
         self.isOpen = true; // open only first time
